dc0e599d452ad45bec746416323cc3640a1d3f64
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-12-31 11:33:00 -0600

    Test harness -- 50 req/sec

--- BB.ScalingAzureFunctions.TestHarness/Program.cs
+++ BB.ScalingAzureFunctions.TestHarness/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
 
 namespace BB.ScalingAzureFunctions.TestHarness
 {
@@ -10,6 +8,22 @@ namespace BB.ScalingAzureFunctions.TestHarness
     {
         static void Main(string[] args)
         {
+            Uri apiUri = new Uri(args[0]);
+
+            ServicePointManager.DefaultConnectionLimit = 100;
+            Console.WriteLine($"Connection limit: {ServicePointManager.DefaultConnectionLimit}");
+
+            while (true)
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    Console.WriteLine($"Creating request @ {DateTime.Now}");
+                    webClient.DownloadStringAsync(apiUri);
+                }
+
+                Thread.Sleep(20);
+            }
         }
     }
 }
+
