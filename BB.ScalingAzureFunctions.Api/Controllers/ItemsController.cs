a29e282f5acf2653c767fa74b6799a654930b9b9
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2016-12-31 11:06:26 -0600

    New controller in API to mirror functions

---
+++ BB.ScalingAzureFunctions.Api/Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Swashbuckle.Swagger.Annotations;
+
+namespace BB.ScalingAzureFunctions.Api.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        // GET api/values
+        [SwaggerOperation("Get")]
+        public async Task<string> Get()
+        {
+            // simulate database query
+            await Task.Run(() => System.Threading.Thread.Sleep(1000));
+
+            // return new item
+            return JsonConvert.SerializeObject(new { Id = Guid.NewGuid() });
+        }
+    }
+}
