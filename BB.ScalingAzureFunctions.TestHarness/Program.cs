b8d5914c6e114744c4d2237b041b3595d387649a
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-01-01 10:12:25 -0600

    Get/Update logic for API, hitting table storage

--- BB.ScalingAzureFunctions.TestHarness/Program.cs
+++ BB.ScalingAzureFunctions.TestHarness/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,26 +8,48 @@ namespace BB.ScalingAzureFunctions.TestHarness
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Uri apiUri = new Uri(args[0]);
+        private static Random _random = new Random();
 
-            ServicePointManager.DefaultConnectionLimit = 100;
+        static void Main(string[] args)
+        {   
             Console.WriteLine($"Connection limit: {ServicePointManager.DefaultConnectionLimit}");
 
             while (true)
             {
                 Console.WriteLine($"Creating request @ {DateTime.Now}");
-                Task.Run(async () =>
-                {
-                    using (WebClient webClient = new WebClient())
-                    {
-                        Console.WriteLine(await webClient.DownloadStringTaskAsync(apiUri));
-                    }
-                });
+                Task.Run(async () => await TestUpdate());
 
-                Thread.Sleep(20);
+                Thread.Sleep(20);   // 1000 / 50 == 20 for 50 req/sec
             }
+        }
+
+        private static async Task TestGet()
+        {
+            int id = _random.Next(1, 100);
+            Console.WriteLine(await Request($"http://bb-scaling-api.azurewebsites.net/api/people?id={id}", "GET"));
+        }
+
+        private static async Task TestUpdate()
+        {
+            int id = _random.Next(1, 100);
+            Console.WriteLine(await Request($"http://bb-scaling-api.azurewebsites.net/api/people?id={id}&lastUpdated={DateTime.Now}", "PATCH"));
+        }
+
+        private static async Task<string> Request(string url, string method)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = method;
+
+            string responseContent = string.Empty;
+
+            using (WebResponse response = await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader sr99 = new StreamReader(stream))
+            {
+                responseContent = await sr99.ReadToEndAsync();
+            }
+
+            return responseContent;
         }
     }
 }
